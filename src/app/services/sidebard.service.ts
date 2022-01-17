e4528b516495e666cc124665b8f6e9c844d54884
Lorwik <lorwik@gmail.com>
2022-01-17 13:45:08 +0100

    Fin sección 8

--- src/app/services/sidebard.service.ts
+++ src/app/services/sidebard.service.ts
@@ -13,6 +13,8 @@ export class SidebardService {
         { titulo:  'Main', url: '/' },
         { titulo:  'Progressbar', url: 'progress' },
         { titulo:  'Graficas', url: 'grafica1' },
+        { titulo:  'Promesas', url: 'promesas' },
+        { titulo:  'Rxjs', url: 'rxjs' },
       ]
     }
   ];
